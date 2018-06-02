217b5b36604ed74ad71e97427ff40e2246f0c370
Mathieu Larue <mat_1794@hotmail.com>
2018-06-02 18:07:04 -0400

    Cleanup

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-var serverPort = 8080;
-var port = process.env.PORT || serverPort;
+var localPort = 8080
+var port = process.env.PORT || localPort
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
