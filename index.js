0fded9ceb6f1bcfa773aac088127062a9bd1542a
Mathieu Larue <mat_1794@hotmail.com>
2018-06-02 18:03:35 -0400

    Attempt to fix port

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 
+var serverPort = 8080;
+var port = process.env.PORT || serverPort;
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
 })
