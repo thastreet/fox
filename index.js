5e7d2b9e86a47b6887b9194b34e5a8e4ef918843
Mathieu Larue <mat_1794@hotmail.com>
2018-06-03 15:01:42 -0400

    Token

--- index.js
+++ index.js
@@ -6,20 +6,41 @@ const localPort = 8080
 const port = process.env.PORT || localPort
 
 const clientId = 26073
-const redirectUri = 'stravastreet://response'
+const clientSecret = process.env.CLIENT_SECRET
+const redirectUri = 'https://strava-street.herokuapp.com/login/response'
 
 app.get('/', function (req, res) {
-  res.send('Suh')
+  res.send('Hello World!')
 })
 
 app.get('/login', function (req, res) {
+  console.log('login')
   res.redirect('https://www.strava.com/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirectUri + '&response_type=code');
 })
 
 app.get('/login/response', function (req, res) {
-  res.send(req.query.code)
+  console.log('response')
+  const code = req.query.code
+
+  request.post(
+    'https://www.strava.com/oauth/token',
+    {
+      json:
+        {
+          client_id: clientId,
+          client_secret: clientSecret,
+          code: code
+        }
+    },
+    function (error, response, body) {
+      console.log('post')
+      if (!error && response.statusCode == 200) {
+        res.redirect('stravastreet://response?access_token=' + response.body.access_token);
+      }
+    }
+  );
 })
 
 app.listen(port, function () {
-  console.log('Strava app listening on port ' + port)
+  console.log('Example app listening on port ' + port)
 })
