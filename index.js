938026ba8a848745f60dcbda2bff73387b671eaf
Mathieu Larue <mat_1794@hotmail.com>
2018-06-03 12:26:53 -0400

    Test

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express')
+const express = require('express');
+
 const app = express()
 
-var localPort = 8080
-var port = process.env.PORT || localPort
+const localPort = 8080
+const port = process.env.PORT || localPort
+
+const clientId = 26073
+const redirectUri = 'http://localhost:8080/login/response'
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.get('/login', function (req, res) {
+  res.redirect('https://www.strava.com/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirectUri + '&response_type=code');
+
+  app.get('/login/response', function (req, res) {
+    console.log('response')
+    console.log(req.query.code)
+    console.log(req.query.scope)
+  })
 })
 
 app.listen(port, function () {
