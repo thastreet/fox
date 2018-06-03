ac60e961b7133d57b9c6ef49ab758b106031fbe8
Mathieu Larue <mat_1794@hotmail.com>
2018-06-03 14:24:50 -0400

    Code

--- index.js
+++ index.js
@@ -6,17 +6,10 @@ const localPort = 8080
 const port = process.env.PORT || localPort
 
 const clientId = 26073
-const clientSecret = process.env.CLIENT_SECRET
 const redirectUri = 'https://strava-street.herokuapp.com/login/response'
 
-var appIdMap = new Map();
-
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.get('/status', function (req, res) {
-  
+  res.send('Suh')
 })
 
 app.get('/login', function (req, res) {
@@ -24,33 +17,9 @@ app.get('/login', function (req, res) {
 })
 
 app.get('/login/response', function (req, res) {
-  const code = req.query.code
-
-  console.log(req)
-  console.log(res)
-
-  request.post(
-    'https://www.strava.com/oauth/token',
-    {
-      json:
-        {
-          client_id: clientId,
-          client_secret: clientSecret,
-          code: code
-        }
-    },
-    function (error, response, body) {
-      const statusCode = response.statusCode
-
-      if (statusCode == 200) {
-        
-      }
-
-      res.sendStatus(statusCode)
-    }
-  );
+  res.send(req.query.code)
 })
 
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port)
+  console.log('Strava app listening on port ' + port)
 })
